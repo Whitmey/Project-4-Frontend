548948c157546cdaa2e70b28ee7bd593d9e51e9b
Will Whitmey <w.whitmey@outlook.com>
2016-12-15 17:29:04 +0000

    heroku commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 angular.module('finalProject', ['ngResource', 'ui.router', 'satellizer'])
-  .constant('API_URL', window.location.hostname === 'localhost' ? 'http://localhost:3000' : 'https://boostme-api.herokuapp.com')
+  .constant('API_URL', window.location.hostname === 'localhost' ? 'http://localhost:3000' : 'https://boostme-api.herokuapp.com/api')
   .config(Auth);
 
 Auth.$inject = ['$authProvider', 'API_URL'];
